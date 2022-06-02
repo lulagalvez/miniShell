3f8e11e79fff809a87ad88f682291b8ea4b83289
Lula Galvez <lulagalvez@github.com>
2022-06-01 23:00:26 -0400

    agregue archivos headers y main

---
+++ src/hashT1.h
@@ -0,0 +1,15 @@
+#ifndef HASHT1_H
+#define HASHT1_H
+#include <iostream>
+
+class hashT1
+{
+    public:
+        hashT1();
+        virtual ~hashT1();
+    protected:
+    private:
+
+};
+
+#endif
---
+++ src/hashT2.h
@@ -0,0 +1,15 @@
+#ifndef HASHT2_H
+#define HASHT2_H
+#include <iostream>
+
+class hashT2
+{
+    public:
+        hashT2();
+        virtual ~hashT2();
+    protected:
+    private:
+
+};
+
+#endif
