e6f25953c27ab86b35f227a078408270f2f4a903
Lula Galvez <lulagalvez@github.com>
2022-09-14 22:25:31 -0300

    agregado funcion de parsing

--- miniS.cpp
+++ miniS.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <sstream>
 
 #define clear() printf("\033[H\033[J")
 
@@ -19,6 +21,27 @@ int getInput(std::string &str)
     }
 }
 
+std::vector<std::vector<std::string> > parseString(std::string str)
+{
+    std::stringstream ss(str);
+    std::vector<std::vector<std::string> > strvVector;
+    strvVector.push_back(std::vector<std::string>());
+
+    std::string temp;
+    int cnt = 0;
+    while(ss >> temp)
+    {
+        if(temp == "|")
+        {
+            strvVector.push_back(std::vector<std::string>());
+            cnt++;
+            continue;
+        }   
+        strvVector[cnt].push_back(temp);
+    }
+    return strvVector;
+}
+
 int main()
 {
     clear();
@@ -28,7 +51,17 @@ int main()
         if(getInput(str))
             continue;
         
-        std::cout << str << "\n";
+        std::vector<std::vector<std::string> > parsedString = parseString(str);
+
+        for(int i = 0; i < parsedString.size(); i++)
+        {
+            for(int j = 0; j < parsedString[i].size(); j++)
+            {
+                std::cout << parsedString[i][j] << ",";
+            }    
+            std::cout << "\n";
+        }
+
     }
 
     return 0;
